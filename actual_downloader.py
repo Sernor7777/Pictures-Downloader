c9f930492a299a04368ce86948175d9143d10e8d
Sernor7777 <122465857+Sernor7777@users.noreply.github.com>
2023-04-06 22:19:47 +0300

    Create actual_downloader.py

---
+++ actual_downloader.py
@@ -0,0 +1,16 @@
+import requests
+
+count = 0
+txt = input("What is the name of your txt file (without .txt and make sure to put the file in the same directory as the script)\n")
+
+with open(rf'{txt}.txt', 'r') as f:
+    content = f.readlines()
+    lines = len(content)
+    while count < lines:
+        url = content[count].replace("\n", "")
+        res = requests.get(url, stream=True)
+        if res.status_code == 200:
+            with open(f"pictures/picture{count}.jpg", 'wb') as f:
+                f.write(res.content)
+        count += 1
+        
